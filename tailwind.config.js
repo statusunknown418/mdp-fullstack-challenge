1009c6b66a502dafd68bcf28ff42af72e40e75b0
Alvaro_Aquije <alvarodevcode@outlook.com>
2022-02-05 22:17:30 -0500

    feat: styling updated and added headlessui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: [],
+  content: ['./ui/**/*.tsx', './pages/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+      },
+      colors: {
+        'border-primary': '#52525b',
+        'text-primary': '#ffffff',
+        'bg-primary': '#18181b',
+        'bg-secondary': '#27272a',
+      },
+      borderRadius: {
+        primary: '0.375rem',
+      },
+    },
   },
   plugins: [],
 }
